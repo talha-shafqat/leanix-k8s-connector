844ee8f24b07b77359a8d3cd6e724106a5ae72f0
Maik Schwan <maik.schwan@leanix.net>
2019-04-04 13:36:22 +0200

    HOOK-417 Add mapping function for nodeInfo -> KubernetesObject

--- kubernetesMapper.go
+++ kubernetesMapper.go
@@ -23,3 +23,18 @@ func NewKubernetesNodeInfo(nodes *corev1.NodeList) KubernetesNodeInfo {
 		NodeTypes:         nodeTypes.Items(),
 	}
 }
+
+// NewClusterKubernetesObject creates a KubernetesObject representing a cluster
+func NewClusterKubernetesObject(clusterName string, nodeInfo KubernetesNodeInfo) KubernetesObject {
+	return KubernetesObject{
+		ID:   clusterName,
+		Type: "cluster",
+		Data: map[string]interface{}{
+			"availabilityZones": nodeInfo.AvailabilityZones,
+			"clusterName":       clusterName,
+			"dataCenter":        nodeInfo.DataCenter,
+			"nodeTypes":         nodeInfo.NodeTypes,
+			"numberNodes":       nodeInfo.NumberNodes,
+		},
+	}
+}
